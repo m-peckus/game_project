2a7eb455f21b971a416e67f2f16252298f36eed1
m-peckus <martinpeckus@gmail.com>
2024-12-09 12:16:23 +0100

    commit 22 asteroid split (after shot) logic implemented

--- png_asteroid.py
+++ png_asteroid.py
@@ -1,53 +1,43 @@
 import pygame
 import random 
-from constants import *
-from circleshape import CircleShape
+from constants import * # Import game constants
+from circleshape import CircleShape # Parent class for circular shapes
 
 class Asteroid(CircleShape):
     def __init__(self, x, y,radius):
+        # Initialize position and radius using the parent class
         super().__init__(x,y, radius)
 
     def draw(self, screen):
-    # Draw a circle on the screen with white color,
-    # centered at self.position, with a radius of self.radius
-    # and a line thickness of 2
+        # Draw the asteroid as a white circle with a 2-pixel border
         pygame.draw.circle(screen, 'white', self.position, self.radius, 2)
     
     def update(self, dt):
-    # Calculate change in position using velocity vector and delta time
+        # Update position based on velocity and time delta
         displacement = self.velocity * dt
-    # Update the position by adding the displacement
         self.position += displacement
 
-
-    #split asteroid if hit by bullet 26/11
     def split(self, screen):
+        # Remove asteroid after being hit by a bullet
         self.kill()
-        if(self.radius <= ASTEROID_MIN_RADIUS):#if smal asteroid is shot
-            print('small asteroid')
+        # If asteroid is too small stop further spliting
+        if(self.radius <= ASTEROID_MIN_RADIUS):
             return
         else:
-            #if big asteroid is shot
+            # Calculate new velocities for split asteroids
             random_angle = random.uniform(20, 50)
-            rotated_vector1 = self.velocity.rotate(random_angle)
-            rotated_vector2 = self.velocity.rotate(-random_angle)
-            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            rotated_vector1 = self.velocity.rotate(random_angle) # Rotate velocity by a positive angle
+            rotated_vector2 = self.velocity.rotate(-random_angle)# Rotate velocity by a negative angle
+            new_radius = self.radius - ASTEROID_MIN_RADIUS # Decrease radius for split asteroids
+            
+            # Create new asteroids at the same position with the new radius and velocities
+            new_asteroid1 = Asteroid(self.position, pygame.Vector2(rotated_vector1), new_radius)
+            new_asteroid2 = Asteroid(self.position, pygame.Vector2(rotated_vector2), new_radius)
+            
+            # Assign calculated velocities to new asteroids and increase their speed by 1.2
+            new_asteroid1.velocity = rotated_vector1 * 1.2
+            new_asteroid2.velocity = rotated_vector2 * 1.2
 
-            new_asteroid1 = Asteroid(self.position, rotated_vector1, new_radius)
-            new_asteroid1
-        """
-            random_angle = random.uniform(20, 50)
-            rotated_vector1 = self.velocity.rotate(random_angle)
-            rotated_vector2 = self.velocity.rotate(-random_angle)
-            new_radius = self.radius - ASTEROID_MIN_RADIUS
 
             
-            print('----------HIT PACKAGE--------')
-            print('random angle',random_angle, -random_angle)
-            print('big asteroid initial velocity', self.velocity)
-            print('new velocity vector-1', rotated_vector1)
-            print('new velocity vector-2', rotated_vector2)
-            print('old radius',self.radius,'new radius',new_radius)
-            print('-------------------------------')
-        """
             
